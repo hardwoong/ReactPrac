256490d27e80b770bb4910e7c85f189cad9b2363
hardwoong <dew04300@naver.com>
2025-02-25 14:29:45 +0900

    update lifecycle func in class-counter

--- ch5/class-counter/src/App.tsx
+++ ch5/class-counter/src/App.tsx
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { ErrorInfo, useState } from 'react';
 import styled from '@emotion/styled';
 import { Button } from 'components/Button';
 import { Label } from 'components/Label';
 import { Component } from 'react';
+import { IScriptSnapshot } from 'typescript';
 
 const Container = styled.div`
   height: 100vh;
@@ -36,6 +37,84 @@ export class App extends Component<Props, State> {
     this.state = {
       counter: 0,
     };
+  }
+
+  private sub = () => {
+    const { counter } = this.state;
+    this.setState({
+      counter: counter - 1,
+    });
+  };
+
+  private add = () => {
+    const { counter } = this.state;
+    this.setState({
+      counter: counter + 1,
+    });
+  };
+
+  render() {
+    const { counter } = this.state;
+
+    return (
+      <Container>
+        <Title>Counter App</Title>
+        <Contents>
+          <Button label='-' onClick={this.sub} />
+          <Label data={counter} />
+          <Button label='+' onClick={this.add} />
+        </Contents>
+      </Container>
+    );
+  }
+
+  static getDerivedStateFromProps(nextProps: Props, prevState: State) {
+    console.log('getDerivedStateFromProps');
+    console.log('nextProps: ', nextProps);
+    console.log('prevState: ', prevState);
+
+    return null;
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  getSnapshotBeforeUpdate(prevProps: Props, prevState: State) {
+    console.log('getSnapshotBeforeUpdate');
+    console.log('prevProps: ', prevProps);
+    console.log('prevState: ', prevState);
+
+    return {
+      testData: true,
+    };
+  }
+
+  componentDidUpdate(prevProps: Props, prevState: State, snapshot: IScriptSnapshot) {
+    console.log('componentDidUpdate');
+    console.log('prevProps: ', prevProps);
+    console.log('prevState: ', prevState);
+    console.log('snapshot: ', snapshot);
+  }
+
+  shouldComponentUpdate(nextProps: Props, nextState: State): boolean {
+    console.log('shouldComponentUpdate');
+    console.log('nextProps: ', nextProps);
+    console.log('nextState: ', nextState);
+    return true;
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+
+  componentDidCatch(error: Error, info: React.ErrorInfo) {
+    console.log('componentDidCatch');
+    console.log('error: ', error);
+    console.log('info: ', info);
+    // this.setState({
+    //   error: true,
+    // });
   }
 }
 
