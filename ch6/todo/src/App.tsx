be8ba204e2ffe1ccf67e00dcf06aa21cb33a52e5
hardwoong <dew04300@naver.com>
2025-02-27 00:00:34 +0900

    add todoitem component

--- ch6/todo/src/App.tsx
+++ ch6/todo/src/App.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 import { Title } from 'components/Title';
-import { Button } from 'components/Button';
+import { ToDoItem } from 'components/ToDoItem';
 
 const Container = styled.div`
   height: 100vh;
@@ -15,7 +15,7 @@ function App() {
   return (
     <Container>
       <Title label='할 일 목록' />
-      <Button label='삭제' />
+        <ToDoItem label='리액트 공부하기' />
     </Container>
   );
 }
