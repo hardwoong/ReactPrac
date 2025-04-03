b8ade23a297a07888a90a4fe23c99d9e17fdb754
hardwoong <dew04300@naver.com>
2025-04-03 15:35:43 +0900

    tmp

--- ch7/react-router-todo/src/App.tsx
+++ ch7/react-router-todo/src/App.tsx
@@ -8,7 +8,7 @@ const Container = styled.div`
   height: 100vh;
   display: flex;
   flex-direction: column;
-  align-items: center;
+  /* align-items: center; */
   justify-content: center;
   background-color: #eeeeee;
 `;
